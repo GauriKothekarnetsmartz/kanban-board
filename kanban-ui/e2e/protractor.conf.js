0e5a713107a4099f5593931228b1a86fee550bbe
GauriKothekarnetsmartz <160697991+GauriKothekarnetsmartz@users.noreply.github.com>
2024-05-17 10:26:28 +0530

    Update protractor.conf.js

--- kanban-ui/e2e/protractor.conf.js
+++ kanban-ui/e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://${IP_ADDRESS}:4200/',
+  baseUrl: 'http://${IP_ADDRESS}:4201/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
